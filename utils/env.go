dcff0be659b7697b5f9c1c357aab3b8ec6fb68ad
Shudi Wang <shudiwsh2009@gmail.com>
2016-02-29 09:57:41 +0800

    fix typos and add sms env

--- utils/env.go
+++ utils/env.go
@@ -1,8 +1,8 @@
 package utils
 
 const (
-	SMS_UID       = "shudiwsh2009"
-	SMS_KEY       = "946fee2e7ad699b065f1"
+	SMS_UID       = "thxlfzzx"
+	SMS_KEY       = "5bb4a1c9cd462c073969"
 	MAIL_SMTP     = "smtp.qq.com"
 	MAIL_USERNAME = "thxlfzzx@qq.com"
 	MAIL_PASSWORD = "zztwhqsqpjeydche"

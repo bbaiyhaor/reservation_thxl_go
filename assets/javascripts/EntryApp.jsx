7e4f874f89c7c1523003f6d39df4216b953094e5
Shudi Wang <shudiwsh2009@gmail.com>
2016-10-27 00:00:04 +0800

    FIX: try to eslint EntryApp.jsx

--- assets/javascripts/EntryApp.jsx
+++ assets/javascripts/EntryApp.jsx
@@ -3,48 +3,56 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Link, Router} from 'react-router';
-import {Panel, PanelHeader, PanelBody, MediaBox, MediaBoxHeader, MediaBoxBody, MediaBoxTitle, MediaBoxDescription} from 'react-weui';
+import { Panel, PanelHeader, PanelBody, MediaBox, MediaBoxHeader, MediaBoxBody, MediaBoxTitle, MediaBoxDescription } from 'react-weui';
 import 'weui';
 
 import PageBottom from '#coms/PageBottom';
 import StudentEntryIcon from '#imgs/mobile/student.png';
 import TeacherEntryIcon from '#imgs/mobile/teacher.png';
 
-class EntryApp extends React.Component{
-    render() {
-        let studentEntryIcon = <img src={StudentEntryIcon}/>;
-        let teacherEntryIcon = <img src={TeacherEntryIcon}/>;
-
-        return (
-            <Panel access>
-                <PanelHeader style={{fontSize: "18px"}}>
-                    学生心理发展指导中心预约系统
-                </PanelHeader>
-                <PanelBody>
-                    <MediaBox type="appmsg" href="/m/student">
-                        <MediaBoxHeader>{studentEntryIcon}</MediaBoxHeader>
-                        <MediaBoxBody>
-                            <MediaBoxTitle>我是学生</MediaBoxTitle>
-                            <MediaBoxDescription>点击进入</MediaBoxDescription>
-                        </MediaBoxBody>
-                    </MediaBox>
-                    <MediaBox type="appmsg" href="/m/teacher">
-                        <MediaBoxHeader>{teacherEntryIcon}</MediaBoxHeader>
-                        <MediaBoxBody>
-                            <MediaBoxTitle>我是咨询师</MediaBoxTitle>
-                            <MediaBoxDescription>点击进入</MediaBoxDescription>
-                        </MediaBoxBody>
-                    </MediaBox>
-                </PanelBody>
-                <PageBottom style={{color: "#999999", textAlign: "center"}}
-                            contents={["清华大学学生心理发展指导中心", "联系方式：010-62782007"]}/>
-            </Panel>
-        );
-    }
+function EntryApp({ studentEntryIcon, teacherEntryIcon }) {
+  return (
+    <Panel access>
+      <PanelHeader style={{ fontSize: '18px' }}>
+        学生心理发展指导中心预约系统
+      </PanelHeader>
+      <PanelBody>
+        <MediaBox type="appmsg" href="/m/student">
+          <MediaBoxHeader>
+            <img src={studentEntryIcon} alt="Student Entry Icon" />
+          </MediaBoxHeader>
+          <MediaBoxBody>
+            <MediaBoxTitle>我是学生</MediaBoxTitle>
+            <MediaBoxDescription>点击进入</MediaBoxDescription>
+          </MediaBoxBody>
+        </MediaBox>
+        <MediaBox type="appmsg" href="/m/teacher">
+          <MediaBoxHeader>
+            <img src={teacherEntryIcon} alt="Teacher Entry Icon" />
+          </MediaBoxHeader>
+          <MediaBoxBody>
+            <MediaBoxTitle>我是咨询师</MediaBoxTitle>
+            <MediaBoxDescription>点击进入</MediaBoxDescription>
+          </MediaBoxBody>
+        </MediaBox>
+      </PanelBody>
+      <PageBottom
+        style={{ color: '#999999', textAlign: 'center' }}
+        contents={['清华大学学生心理发展指导中心', '联系方式：010-62782007']}
+      />
+    </Panel>
+  );
 }
 
+EntryApp.propTypes = {
+  studentEntryIcon: React.PropTypes.string.isRequired,
+  teacherEntryIcon: React.PropTypes.string.isRequired,
+};
+
 ReactDOM.render(
-    React.createElement(EntryApp, null),
-    document.getElementById('content')
+  <EntryApp
+    studentEntryIcon={StudentEntryIcon}
+    teacherEntryIcon={TeacherEntryIcon}
+  />,
+  document.getElementById('content')
 );
